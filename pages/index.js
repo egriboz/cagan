37f2d6ec4007c9321256627f2f68ebf7ce227cee
fatih egriboz <fatih.egriboz@bilgi.edu.tr>
2021-04-09 19:59:30 +0300

    add to title. I am Çağan

--- pages/index.js
+++ pages/index.js
@@ -4,15 +4,26 @@ import Bird from "./birds";
 
 const Index = () => {
   return (
-    <div className="main">
-      <Bird />
-      {/* <Link href="/birds">
+    <>
+      <div className="main">
+        <div className="logo">DRAGON MAVİŞ</div>
+        <Bird />
+        {/* <Link href="/birds">
         <a>Birds Example</a>
       </Link>
       <Link href="/boxes">
         <a>Boxes Example</a>
       </Link> */}
-    </div>
+      </div>
+      <style jsx global>{`
+        .logo {
+          padding: 10px;
+          color: darkblue;
+          font-family: sans-serif;
+          font-weight: bold;
+        }
+      `}</style>
+    </>
   );
 };
 
